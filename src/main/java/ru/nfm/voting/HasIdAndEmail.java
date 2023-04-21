28a5607b538ced1fa290b594faf81c7649405624
NFM <daniel.olovianniy@gmail.com>
2023-04-22 00:36:54 +0300

    Initial Commit

---
+++ src/main/java/ru/nfm/voting/HasIdAndEmail.java
@@ -0,0 +1,5 @@
+package ru.nfm.voting;
+
+public interface HasIdAndEmail extends HasId {
+    String getEmail();
+}
---
+++ src/main/java/ru/nfm/voting/error/AppException.java
@@ -0,0 +1,10 @@
+package ru.nfm.voting.error;
+
+import org.springframework.lang.NonNull;
+
+public class AppException extends RuntimeException {
+
+    public AppException(@NonNull String message) {
+        super(message);
+    }
+}
